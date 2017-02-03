169a6b7de8db0fdb4e6da0a05a34788281d22b97
anthonielance <alance33@gmail.com>
2017-02-03 07:41:14 -0500

    Add Talk controller and model

---
+++ MyCodeCamp/Models/TalkUrlResolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyCodeCamp.Controllers;
+using MyCodeCamp.Data.Entities;
+
+namespace MyCodeCamp.Models
+{
+    public class TalkUrlResolver : IValueResolver<Talk,TalkModel,string>
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public TalkUrlResolver(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string Resolve(Talk source,TalkModel destination,string destMember,ResolutionContext context)
+        {
+            var url = (IUrlHelper)_httpContextAccessor.HttpContext.Items[BaseController.URLHELPER];
+            return url.Link("TalkGet",new { moniker = source.Speaker.Camp.Moniker, speakerId = source.Speaker.Id, id = source.Id });
+        }
+    }
+}
